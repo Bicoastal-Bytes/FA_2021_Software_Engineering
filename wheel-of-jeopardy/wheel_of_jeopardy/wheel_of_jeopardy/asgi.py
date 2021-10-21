6a4a886bd9945cc4cf2e3bfad745835a2ac4f28a
Clayton Franklin <cfrank31@jh.edu>
2021-10-20 20:36:02 -0700

    adding websocket routing

--- wheel-of-jeopardy/wheel_of_jeopardy/wheel_of_jeopardy/asgi.py
+++ wheel-of-jeopardy/wheel_of_jeopardy/wheel_of_jeopardy/asgi.py
@@ -11,15 +11,20 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 # Django Apps Modules
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 # User Created Apps
+import websockets.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wheel_of_jeopardy.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websockets.routing.websocket_urlpatterns
+        )
+    )
 })
-
-ASGI_APPLICATION = "wheel_of_jeopardy.asgi.application"
