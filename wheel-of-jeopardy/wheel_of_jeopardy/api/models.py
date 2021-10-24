e2185e4eca4d9b2d81f3a81de38d6e039081f491
Clayton Franklin <cfrank31@jh.edu>
2021-10-23 20:42:39 -0700

    return strings from model

--- wheel-of-jeopardy/wheel_of_jeopardy/api/models.py
+++ wheel-of-jeopardy/wheel_of_jeopardy/api/models.py
@@ -6,14 +6,22 @@ class Category(models.Model):
     """Model representing the category that a Question is part of"""
     name = models.CharField("Category Name", max_length=60)
 
+    def __str__(self):
+        return self.name
+
 class Question(models.Model):
     """Model representing a Question stored in the database"""
     category = models.ForeignKey(Category, on_delete=CASCADE)
     question = models.CharField("Question", max_length=1000)
     point_value = models.IntegerField("Question Point Value")
 
+    def __str__(self):
+        return self.question
+
 class Choices(models.Model):
     """Model representing the choices that a user has for questions"""
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
     choices = models.JSONField("Choices")
     correct_answer = models.CharField("Correct Answer", max_length=300)
+
+
