66a60d36286284333c5213052e8bbbc853e688a8
Clayton Franklin <cfrank31@jh.edu>
2021-11-05 20:12:08 -0700

    adding new forms

--- wheel-of-jeopardy/wheel_of_jeopardy/websockets/forms.py
+++ wheel-of-jeopardy/wheel_of_jeopardy/websockets/forms.py
@@ -2,10 +2,32 @@
 from django import forms
 
 class RegisterForm(forms.Form):
+    """Form to register a player to a game"""
     player_name = forms.CharField(label="Player Name", max_length=100)
     room_name = forms.CharField(label="Room Name", max_length=100)
 
     def __init__(self, *args, **kwargs):
+        """Basically here to make things look nice"""
         super(RegisterForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+class CreationForm(forms.Form):
+    """Form to choose how many players """
+    CHOICES = [
+        ('6', 6),
+        ('12', 12),
+        ('18', 18),
+        ('24', 24),
+        ('30', 30)
+    ]
+
+    question_count = forms.ChoiceField(label="Number of Questions", choices=CHOICES)
+    player_name = forms.CharField(label="Player Name", max_length=100)
+    room_name = forms.CharField(label="Room Name", max_length=100)
+
+    def __init__(self, *args, **kwargs):
+        """Basically here to make things look nice"""
+        super(CreationForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
