008b77f1f5a083a723ea78a85756166e49edae8b
Clayton Franklin <cfrank31@jh.edu>
2021-11-05 20:12:38 -0700

    adding new views

--- wheel-of-jeopardy/wheel_of_jeopardy/websockets/views.py
+++ wheel-of-jeopardy/wheel_of_jeopardy/websockets/views.py
@@ -1,11 +1,26 @@
-import re
+import pickle
+import redis
 from django.shortcuts import render
-from .forms import RegisterForm
+from .forms import RegisterForm, CreationForm
+
+client = redis.client.StrictRedis()
 
 # Create your views here.
 def index(request):
+    return render(request, 'websockets/index.html')
+
+def create_game(request):
+    form = CreationForm()
+    return render(request, 'websockets/create_game.html', {'form': form})
+
+def join_game(request):
     form = RegisterForm()
-    return render(request, 'websockets/index.html', {'form': form})
+    available_games = pickle.loads(client.get('games'))
+    context = {
+        'form': form,
+        'games': available_games
+    }
+    return render(request, 'websockets/join_game.html', context)
 
 def game_room(request, game_room, user_name):
     return render(request, 'websockets/game.html', {
