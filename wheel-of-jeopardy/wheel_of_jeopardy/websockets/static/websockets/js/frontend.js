e71cf0ed82637da4dc8956b173d1a6a7f15ec337
laferger <35469603+laferger@users.noreply.github.com>
2021-12-10 18:28:58 -0500

    fixed timer

--- wheel-of-jeopardy/wheel_of_jeopardy/websockets/static/websockets/js/frontend.js
+++ wheel-of-jeopardy/wheel_of_jeopardy/websockets/static/websockets/js/frontend.js
@@ -218,21 +218,17 @@ function sendPoints (pointValue) {
 /* countdown timer for the buzzer screen */
 
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-    
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-        display.textContent = seconds;
-    
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+    var timeleft = 5;
+    var downloadTimer = setInterval(function(){
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById("time").innerHTML = "0";
+    } else {
+        document.getElementById("time").innerHTML = timeleft;
     }
+    timeleft -= 1;
+    }, 1000);    
+}
 
     
 function turnOff () {
